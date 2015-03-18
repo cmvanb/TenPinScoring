6d623f4e78ceabdd1663dcfde8a5cbb03d704dfc
Casper van Beuzekom <cmvanb@gmail.com>
2015-03-18 23:12:35 +0100

    clean up

--- js/main.js
+++ js/main.js
@@ -68,49 +68,6 @@ function main()
     userInteraction.setPregameElementsVisible(true);
     userInteraction.setIngameElementsVisible(false);
     userInteraction.setEndgameElementsVisible(false);
-
-    // Test data/code
-    /*
-    var perfectGameData = [
-        10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-        10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-        10, 10, 10, 10];
-
-    var sampleGameData = [
-        9, 1, 3, 7, 3, 7, 10, 10, 2, 1,
-        6, 4, 2, 3, 10, 10, 10, 10, 10, 2,
-        1, 10, 3, 6, 4, 3, 7, 3, 1, 3, 7, 2];
-
-    var sampleGameData2 = [
-        0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10,
-        0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10, 0, 0, 10,
-        10, 10];
-
-    autoplayInterval(game, perfectGameData);
-    */
-};
-/*
-function autoplayInstant(game, gameData)
-{
-    while (!game.gameOver)
-    {
-        game.bowl(gameData.shift());
-    }
 };
 
-function autoplayInterval(game, gameData)
-{
-    function next()
-    {
-        game.bowl(gameData.shift());
-
-        if (game.gameOver)
-        {
-            window.clearInterval(interval);
-        }
-    }
-
-    var interval = window.setInterval(next, 500);
-};
-*/
 main();
